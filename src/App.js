f81583efafe28e00ed2a871b12bac10df28f7931
Vlad Neculai <vladut.neculai@fabfab.com>
2020-04-03 16:22:43 +0200

    react-modal installed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import API_KEY from './API_KEY';
 
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import Modal from 'react-responsive-modal';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      open : false,
       restaurantsNearby: [],
       lat: null,
       lng: null,
@@ -22,6 +24,14 @@ class App extends Component {
     };
   }
   _isMounted = false;
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+ 
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
   componentDidMount() {
     this.getBrowserLocation()
@@ -39,35 +49,63 @@ class App extends Component {
       });
   }
   onRestaurantFormSubmit() {
-    this.setState({
-      restaurantsNearby: [{
+    // this.setState({
+    //   restaurantsNearby: [
+    //     {
+    //       name: "Gica",
+    //       formattedAddress: formattedAddress,
+    //       geometry: {
+    //         location: {
+    //           lat: x,
+    //           lng: y
+    //         }
+    //       },
+    //       restaurantId: 2,
+    //       rating: 3,
+    //       reviews: [
+    //         {
+    //           author_name: "vlad",
+    //           rating: 5,
+    //           text: "very clean, nice"
+    //         }
+    //       ]
+    //     },
+    //     ...this.state.restaurantsNearby
+    //   ]
+    // });
+  }
+
+  onMapClickChange(lat, lng, formattedAddress) {
+    this.onOpenModal()
+    // this.setState({
+    //   newRestaurantAddress: formattedAddress,
+    //   newRestaurantLat: lat,
+    //   newRestaurantLng: lng
+    // });
+        this.setState({
+      restaurantsNearby: [
+        {
           name: "Gica",
           formatted_address: formattedAddress,
           geometry: {
             location: {
-              lat: x,
-              lng: y
+              lat: lat,
+              lng: lng
             }
           },
           restaurantId: 2,
           rating: 3,
-          reviews: [{
-            author_name: "vlad",
-            rating: 5,
-            text: "very clean, nice"
-          }]
+          reviews: [
+            {
+              author_name: "vlad",
+              rating: 5,
+              text: "very clean, nice"
+            }
+          ]
         },
         ...this.state.restaurantsNearby
-      ],
-    })
-  }
-  
-  onMapClickChange(lat, lng, formattedAddress) {
-    this.setState({
-      newRestaurantAddress: formattedAddress,
-      newRestaurantLat : lat,
-      newRestaurantLng : lng
-    })
+      ]
+    });
   }
 
   getBrowserLocation() {
@@ -75,33 +113,60 @@ class App extends Component {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
   }
-  getStreetView(lat,lng){
-    let url = `https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${lat},${lng}&heading=0&pitch=-0.76&key=${API_KEY}`
+  getStreetView(lat, lng) {
+    let url = `https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${lat},${lng}&heading=0&pitch=-0.76&key=${API_KEY}`;
     return axios.get(url).then(response => {
-      return response.config.url
-    })
+      return response.config.url;
+    });
   }
-  
+
   getNearbyPlaces() {
-    const radius = 300;
+    const radius = 80;
     const { lat, lng } = this.state;
 
-    axios.get(`${"https://cors-anywhere.herokuapp.com/"}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=restaurant&key=${API_KEY}`,{
-      headers: { "Access-Control-Allow-Origin": "http://localhost:3000/" }
-        })
-    .then(response => {
-      response.data.results.map(item => {
-        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://maps.googleapis.com/maps/api/place/details/json?placeid=${item.place_id}&key=${API_KEY}`, {headers:{'Access-Control-Allow-Origin': 'http://localhost:3000/'}})
-        .then(response => {
-         this.setState(prevState=>({restaurantsNearby:[...prevState.restaurantsNearby, response.data.result]}))
-        })
-      })
-    })
+    axios
+      .get(
+        `${"https://cors-anywhere.herokuapp.com/"}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=restaurant&key=${API_KEY}`,
+        {
+          headers: { "Access-Control-Allow-Origin": "http://localhost:3000/" }
+        }
+      )
+      .then(response => {
+        response.data.results.map(item => {
+          axios
+            .get(
+              `${"https://cors-anywhere.herokuapp.com/"}https://maps.googleapis.com/maps/api/place/details/json?placeid=${
+                item.place_id
+              }&key=${API_KEY}`,
+              {
+                headers: {
+                  "Access-Control-Allow-Origin": "http://localhost:3000/"
+                }
+              }
+            )
+            .then(response => {
+              this.setState(prevState => ({
+                restaurantsNearby: [
+                  ...prevState.restaurantsNearby,
+                  response.data.result
+                ]
+              }));
+            });
+        });
+      });
   }
 
   render() {
+    const {open} = this.state;
     return (
       <div>
+        <Modal 
+          open={open}
+          closeIconSize={14}
+          onClose={this.onCloseModal}
+          center
+          >
+        </Modal>
         <Grid
           container
           direction="row"
@@ -109,13 +174,16 @@ class App extends Component {
           alignItems="flex-start"
         >
           <Grid item md={6}>
-            <MapContainer 
+            <MapContainer
               restaurantsNearby={this.state.restaurantsNearby}
-              onMapClickChange={(x, y, info) => this.onMapClickChange(x, y, info)} 
+              onMapClickChange={(x, y, info) =>
+                this.onMapClickChange(x, y, info)
+              }
               getStreetView={this.getStreetView}
-              lat={this.state.lat} 
-              lng={this.state.lng} 
-              zoom={16} />
+              lat={this.state.lat}
+              lng={this.state.lng}
+              zoom={16}
+            />
           </Grid>
 
           <Grid item md={6}>
--- src/components/restaurantsAPI.js
+++ src/components/restaurantsAPI.js
@@ -3,7 +3,6 @@ import cx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Rating from '@material-ui/lab/Rating';
